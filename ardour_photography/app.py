9f4441c13b5536d8552118ec100dc27327ea780e
Denise Lee <denise.lee.90@gmail.com>
2020-07-23 03:28:57 +0000

    create read route

--- ardour_photography/app.py
+++ ardour_photography/app.py
@@ -14,6 +14,10 @@ DB_NAME = "ardour_photography"
 
 client = pymongo.MongoClient(MONGO_URI)
 
+@app.route('/photos')
+def show_all_photos():
+    all_photos = client[DB_NAME].photo.find()
+    return render_template('show_photos.template.html', all_photos=all_photos)
 
 @app.route('/photo/create')
 def create_photo():
@@ -25,12 +29,26 @@ def process_create_photo():
     photographer_name = request.form.get('photographer_name')
     image_location = request.form.get('image_location')
     image_year = request.form.get('image_year')
+    photograph_description = request.form.get('photograph_description')
+    camera_make = request.form.get('camera_make')
+    camera_model = request.form.get('camera_model')
+    focal_length = request.form.get('focal_length')
+    aperture = request.form.get('aperture')
+    shutter_speed = request.form.get('shutter_speed')
+    iso = request.form.get('iso')
 
     client[DB_NAME].photo.insert_one({
         "title": image_name,
         "photographer": photographer_name,
         "location": image_location,
-        "year": image_year
+        "year": image_year,
+        "description": photograph_description,
+        "cameraMake": camera_make,
+        "cameraModel": camera_model,
+        "focalLength": focal_length,
+        "aperture": aperture,
+        "shutterSpeed": shutter_speed,
+        "iso": iso
     })
 
     return "New Photograph Saved"
